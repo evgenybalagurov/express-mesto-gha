f59f95d3bf748e6791b44fa1fc265ce6ccd30b5c
Evgeniy <evgeniy.balagurov@yandex.ru>
2022-08-19 11:33:12 +0400

    create autorization

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62ff379803eee79221a77512',
+  };
+
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use('/users', require('./routes/users'));
